9389dd26d10c5e9fa3bb6089d75ecc28ae900d95
msiegenthaler <msiegenthaler@gmx.net>
2011-10-04 21:55:41 +0200

    add sideEffect annotation

---
+++ src/main/scala/pusca/sideEffect.scala
@@ -0,0 +1,7 @@
+package pusca
+
+/**
+ * Mark a method/function return value as being accompanied with side-effects.
+ * This is equal to marking the method with @impure. 
+ */
+class sideEffect extends StaticAnnotation
