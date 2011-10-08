c2b2e08bc7582920c64e495d6da7f26ab47ca5ca
msiegenthaler <msiegenthaler@gmx.net>
2011-10-08 12:17:50 +0200

    update to new syntax

--- src/test/scala/pusca/test/ScalaLibraryCallsTest.scala
+++ src/test/scala/pusca/test/ScalaLibraryCallsTest.scala
@@ -3,17 +3,17 @@ package pusca.test
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
 import org.junit.Test
+import PluginTester._
+
 
 class ScalaLibraryCallsTest extends JUnitSuite with ShouldMatchersForJUnit {
 
   @Test def callToExternalImpureFunctionPrintlnInsidePureFunction {
-    val e = PluginTester("def b(a: String) = println(a)").compileErrors
-    e should have size (1)
-    println(e.head)
-    e.head should include("Impure function call to scala.Predef.println inside the pure function b")
+    code("def b(a: String) = println(a)") should
+    	yieldCompileError("impure function call inside the pure function 'b'")
   }
 
   @Test def callToExternalImpureFunctionPrintlnInsideImpureFunction {
-    PluginTester("@impure def b(a: String) = println(a)") should be('compiled)
+    code("@impure def b(a: String) = println(a)") should compile
   }
 }
