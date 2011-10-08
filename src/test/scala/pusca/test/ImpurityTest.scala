e20dc5f7e8dce58dd9b3fdf7a14c9fe1571dda08
msiegenthaler <msiegenthaler@gmx.net>
2011-10-08 12:09:17 +0200

    split PurityTest into purity and impurity and rewrote to use new syntax

---
+++ src/test/scala/pusca/test/ImpurityTest.scala
@@ -0,0 +1,81 @@
+package pusca.test
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import PluginTester._
+import org.junit.Test
+
+class ImpurityTest extends JUnitSuite with ShouldMatchersForJUnit {
+  def code(s: String) = {
+    new PluginTester().
+      fromString("@impure def ip(i: Int) = 10").
+      fromString("@pure def p(i: Int) = i").
+      fromString(s).run
+  }
+
+  @Test def impureFunctionMayCallAPureFunction {
+    code("@impure def a = p(10)") should compile
+  }
+  
+  @Test def impureFunctionMayCallAnImpureFunction {
+    code("@impure def a = ip(10)") should compile
+  }
+  
+  @Test def impureFunctionMayUseLocalVars {
+    code("""
+        @impure def a = {
+        	var x = 10
+          x = x + 1
+          x
+        }
+      """) should compile
+  }
+  
+  @Test def impureFunctionMayAccessVars {
+    code("""
+  	     class A {
+  	       var x: Int = 0
+  			   @impure def a = x
+         }
+  	  """) should compile
+  }
+
+  @Test def impureFunctionMayModifyVars {
+    code("""
+  			 class A {
+  			  var x: Int = 0
+  			  @impure def a(i: Int) { x = i }
+  			 }
+  	  """) should compile
+  }
+
+  @Test def impureFunctionMayAccessVals {
+    code("""
+        class A {
+        	val x = 1021
+        	@impure def a = x
+    		}
+      """) should compile
+  }
+  
+  @Test def impureFunctionMayAccessVarInOtherClasses {
+    code("""
+        object A { var a = 0 }
+        class B {
+        	@impure def x = A.a + 10
+        }
+      """) should compile
+  }
+
+  @Test def impureFunctionMayWriteVarInOtherClasses {
+    code("""
+        object A { var a = 0 }
+        class B {
+        	@impure def x = {
+        		A.a = A.a + 1
+        		A.a
+    			}
+        }
+      """) should compile
+  }
+}
--- src/test/scala/pusca/test/PurityTest.scala
+++ src/test/scala/pusca/test/PurityTest.scala
@@ -3,111 +3,125 @@ package pusca.test
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
 import org.junit.Test
+import org.scalatest.matchers.Matcher
+import org.scalatest.matchers.MatchResult
+import PluginTester._
 
 class PurityTest extends JUnitSuite with ShouldMatchersForJUnit {
 
-  @Test def defPureFunctionDouble {
-    PluginTester("def double(x: Int) = x * 2") should be('compiled)
+  def code(s: String) = {
+    new PluginTester().fromString("@impure def ip(i: Int) = 10").fromString(s).run
   }
 
-  @Test def defPureFunctionSum {
-    PluginTester("def sum(a: Int, b: Int) = a + b") should be('compiled)
+  @Test def pureFunctionMustNotCallImpureFunction {
+    code("@pure def a = ip(10)") should yieldCompileError("impure function call inside the pure function 'a'")
   }
 
-  @Test def defPureFunctionSumOfList {
-    PluginTester("def sum(e: List[Int]) = e.foldLeft(0)(_ + _)") should be('compiled)
+  @Test def pureFunctionMayCallAPureFunctions {
+    code("""
+        @pure def a(i: Int) = i + 10
+        @pure def b(i: Int) = a(i)
+      """) should compile
   }
 
-  @Test def defPureFunctionThatCallsAnotherPureFunction {
-    PluginTester("""
-    		def a(v: String) = v
-    		def b = a("Hi")
-    	""") should be('compiled)
+  @Test def pureFunctionMayCallMultipleOtherPureFunctionsAndUseVals {
+    code("""
+        @pure def sum(a: Int, b: Int) = a + b
+    		@pure def square(i: Int) = i * i
+        @pure def b(i: Int) = {
+        	val sq = square(i)
+        	val s = sum(i, sq)
+        	s
+        }
+      """) should compile
   }
 
-  @Test def defPureFunctionThatTriesToCallAnImpureOne {
-    val e = PluginTester("""
-    		@impure def a(v: String) = v
-    		def b = a("Hi")
-    	""").compileErrors
-    e should have size (1)
-    e.head should include("Impure function call to a inside the pure function b")
+  @Test def pureFunctionMayHaveInnerPureFunction {
+    code("""
+        	@pure def a(i: Int) = {
+		        def sum(a: Int, b: Int) = a + b
+		    		def square(i: Int) = i * i
+        		sum(square(i), i)
+    			}
+        """) should compile
   }
 
-  @Test def defImpureFunctionThatCallsAnotherImpureFunction {
-    PluginTester("""
-    		@impure def a(v: String) = v
-    		@impure def b = a("Hi")
-    	""") should be('compiled)
+  @Test def pureFunctionMustNotAccessVars {
+    code("""
+  	     class A {
+  	       var x: Int = 0
+  			   @pure def a = x
+         }
+  	  """) should yieldCompileError("access to non-local var inside the pure function 'a'")
+  }
+
+  @Test def pureFunctionMustNotModifyVars {
+    code("""
+  			 class A {
+  			  var x: Int = 0
+  			  @pure def a(i: Int) { x = i }
+  			 }
+  	  """) should yieldCompileError("write to non-local var inside the pure function 'a'")
+  }
+
+  @Test def pureFunctionMustNotAccessVarInOtherClasses {
+    code("""
+        object A { var a = 0 }
+        class B {
+        	@pure def x = A.a + 10
+        }
+      """) should yieldCompileError("access to non-local var inside the pure function 'x'")
+  }
+
+  @Test def pureFunctionMustNotWriteVarInOtherClasses {
+    code("""
+        object A { var a = 0 }
+        class B {
+        	@pure def x = {
+        		A.a = A.a + 1
+        		A.a
+    			}
+        }
+      """) should yieldCompileError("write to non-local var inside the pure function 'x'")
   }
 
   @Test def pureFunctionMayAccessVals {
-    PluginTester("""
-    		val factor = 3
-    		def applyFactor(i: Int) = i * factor""") should be('compiled)
+    code("""
+        class A {
+        	val x = 1021
+        	@pure def a = x
+    		}
+      """) should compile
   }
 
-  @Test def pureFunctionMayNotAccessVars {
-    val e = PluginTester("""
-    		var a = 0
-    		def number = a""").compileErrors
-    e should have size (1)
-    e.head should include("Impure function call")
+  @Test def pureFunctionMayAccessLocalVars {
+    code("""
+        @pure def a(i: Int) = {
+        	var x = i
+        	x = i + 10
+        	x
+        }
+      """) should compile
   }
 
-  @Test def pureFunctionMayNotWriteVars {
-    val e = PluginTester("""
-  			var a = 0
-  			def setA(v: Int) { a = v }""").compileErrors
-    e should have size (1)
-    e.head should include("Impure function call")
+  @Test def pureFunctionMayNotAccessVarsOfOuterFunction {
+    code("""
+        @pure def a = {
+          var x = 10
+          @pure def b = x + 1
+        	b
+    	  }
+      """) should yieldCompileError("access to non-local var inside the pure function 'b'")
   }
 
-  @Test def impureFunctionMayAccessVars {
-    PluginTester("""
-    		var a = 0
-    		@impure def number = a""") should be('compiled)
+  @Test def pureFunctionMayNotWriteVarsOfOuterFunction {
+    code("""
+  			@pure def a = {
+  			  var x = 10
+  		  	@pure def b { x = x + 1 }
+  		  	b
+  	    	x
+  			}
+  	  """) should yieldCompileError("write to non-local var inside the pure function 'b'")
   }
-
-  @Test def impureFunctionMayWriteVars {
-    PluginTester("""
-    		var a = 0
-    		@impure def addA { a = a + 1 }""") should be('compiled)
-  }
-
-  @Test def pureFunctionMayNotAccessVarsInOtherClasses {
-    val e = PluginTester("""
-    		object A { var a = 0 }
-    		object B { def number = A.a }""").compileErrors
-    e should have size (1)
-    e.head should include("Impure function call")
-  }
-
-  @Test def pureFunctionMayNotWriteVarsInOtherClasses {
-    val e = PluginTester("""
-  			object A { var a = 0 }
-  			object B { def setA(v: Int) { A.a = v } }""").compileErrors
-    e should have size (1)
-    e.head should include("Impure function call")
-  }
-
-  @Test def impureFunctionMayAccessVarsInOtherClasses {
-    PluginTester("""
-    		object A { var a = 0 }
-    		object B { @impure def number = A.a }""") should be('compiled)
-  }
-
-  @Test def impureFunctionMayWriteVarsInOtherClasses {
-    PluginTester("""
-    		object A { var a = 0 }
-    		object B { @impure def addA { A.a = A.a + 1 } }""") should be('compiled)
-  }
-
-  @Test def declarePureMakesImpurePure {
-    PluginTester("""
-    		@impure def a = 10
-    		@declarePure def b = a
-    		def c = b""") should be('compiled)
-  }
-
 }
