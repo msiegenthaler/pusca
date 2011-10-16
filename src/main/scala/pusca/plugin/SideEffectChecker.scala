6c0329af7b107b86a4c8e42ed259055e67f2c676
msiegenthaler <msiegenthaler@gmx.net>
2011-10-16 11:18:46 +0200

    add handling of type parameters

--- src/main/scala/pusca/plugin/SideEffectChecker.scala
+++ src/main/scala/pusca/plugin/SideEffectChecker.scala
@@ -8,13 +8,44 @@ abstract class SideEffectChecker {
   import global._
 
   object checker extends AnnotationChecker {
-    private val sideEffectAnnotation = definitions.getClass("pusca.sideEffect")
-    private def hasSideEffect(tpe: Type) = tpe.annotations.find(_.atp.typeSymbol == sideEffectAnnotation).isDefined
+    private val sideEffectAnnotationType = definitions.getClass("pusca.sideEffect")
+    private def hasSideEffect(tpe: Type) = tpe.annotations.find(_.atp.typeSymbol == sideEffectAnnotationType).isDefined
+    private def sideEffectAnnotation = AnnotationInfo(sideEffectAnnotationType.tpe, Nil, Nil)
+    private def withSideEffect(tpe: Type) = if (hasSideEffect(tpe)) tpe else tpe.withAnnotations(sideEffectAnnotation :: tpe.annotations)
 
     def annotationsConform(tpe1: Type, tpe2: Type): Boolean = {
-      val se1 = hasSideEffect(tpe1)
-      val se2 = hasSideEffect(tpe2)
-      se2 || !se1
+      //      println("# conform tp1=" + tpe1 + "    tp2=" + tpe2)
+      hasSideEffect(tpe2) || !hasSideEffect(tpe1)
+    }
+
+    override def annotationsGlb(tp: Type, ts: List[Type]): Type = {
+      //      println("# Glb tp=" + tp + "    ts=" + ts)
+      tp
+    }
+
+    override def annotationsLub(tp: Type, ts: List[Type]): Type = {
+      //      println("# Lub tp=" + tp + "    ts=" + ts)
+      tp
+    }
+
+    override def canAdaptAnnotations(tree: Tree, mode: Int, pt: Type): Boolean = {
+      //      println("# canAdapt  tree=" + tree + "  mode=" + mode + "  pt=" + pt)
+      false
+    }
+
+    override def adaptAnnotations(tree: Tree, mode: Int, pt: Type): Tree = {
+      //      println("# adapt  tree=" + tree + "  mode=" + mode + "  pt=" + pt)
+      tree
+    }
+
+    override def adaptBoundsToAnnotations(bounds: List[TypeBounds],
+                                          tparams: List[Symbol], targs: List[Type]): List[TypeBounds] = {
+      //      println("# Bounds bounds=" + bounds + "   tparams" + tparams + "    " + targs)
+      bounds.zip(targs).map { e ⇒
+        val (bound, targ) = e
+        if (hasSideEffect(targ)) TypeBounds(withSideEffect(bound.lo), withSideEffect(bound.hi))
+        else bound
+      }
     }
   }
 }
--- src/test/scala/pusca/test/SideEffectAnnotationTest.scala
+++ src/test/scala/pusca/test/SideEffectAnnotationTest.scala
@@ -38,4 +38,47 @@ class SideEffectAnnotationTest extends JUnitSuite with ShouldMatchersForJUnit {
         def c: Int = b""") should yieldCompileError("type mismatch")
   }
 
+  @Test def sideEffectIsAValidTypeParameter {
+    code("new Function0[Unit @sideEffect] { override def apply() = () }") should compile
+  }
+  @Test def sideEffectIsAValidTypeParameter2 {
+    code("new Function0[String @sideEffect] { override def apply() = \"Ho\" }") should compile
+  }
+
+  @Test def sideEffectIsAValidTypeParameterForMethod {
+    code("""
+        def a[A](a: A) = 10
+        def b = a[String @sideEffect]("hi")
+        """) should compile
+  }
+
+  @Test def sideEffectIsAValidFunctionTypeParamter {
+    code("new Function1[Int,String @sideEffect] { override def apply(i: Int) = \"hi\" }") should compile
+  }
+
+  @Test def pureReturnTypeConformsToSideEffectTypeParameter {
+    code("new Function1[Int, String @sideEffect] { override def apply(i: Int): String = \"hi\" }") should compile
+  }
+
+  @Test def sideEffectReturnTypeDoesNotConformToPureTypeParameter {
+    code("new Function1[Int, String] { override def apply(i: Int): String @sideEffect = \"hi\" }") should
+      yieldCompileError("overriding method apply in trait Function1")
+  }
+
+  @Test def sideEffectInTypeParameterIsPreserved {
+    //not sure why exactly we need to call a as a{String @sideEffect]. Must be something with the type inference.
+    code("""
+          def a[A](f: Int => A): A = f(10)
+          def b = a[String @sideEffect](new Function1[Int,String @sideEffect] {
+          		override def apply(i: Int) = i.toString
+      			})
+          """) should compile
+    code("""
+          def a[A](f: Int => A): A = f(10)
+          def b = a[String @sideEffect](new Function1[Int,String @sideEffect] {
+          		override def apply(i: Int) = i.toString
+      			})
+          def c: String = b 
+          """) should yieldCompileError("type mismatch")
+  }
 }
