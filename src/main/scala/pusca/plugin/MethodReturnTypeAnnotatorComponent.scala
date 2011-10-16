e1b10f218b14e4b7b2b413c26f62eb606c4873b3
msiegenthaler <msiegenthaler@gmx.net>
2011-10-16 18:07:24 +0200

    implement @impure on classes (make constructor return @sideEffect)

--- src/main/scala/pusca/plugin/MethodReturnTypeAnnotatorComponent.scala
+++ src/main/scala/pusca/plugin/MethodReturnTypeAnnotatorComponent.scala
@@ -17,34 +17,48 @@ class MethodReturnTypeAnnotatorComponent(val global: Global) extends PluginCompo
   def newTransformer(unit: CompilationUnit) = new MethodReturnTypeAnnotator
 
   class MethodReturnTypeAnnotator extends Transformer {
-    def annotationName(t: Tree): Option[Name] = t match {
+    protected def annotationName(t: Tree): Option[Name] = t match {
       case Apply(Select(New(Ident(tpe)), method), args) ⇒ Some(tpe)
       case _ ⇒ None
     }
-    def makeAnnotation(annot: Symbol): Tree = {
+    protected def makeAnnotation(annot: Symbol): Tree = {
       Apply(Select(New(Ident(annot.name)), mkTermName("<init>")), Nil)
     }
 
-    def sameName(annot: Symbol)(cand: Name) = cand == annot.name
-    def hasAnnotation(d: DefDef, annot: Symbol): Boolean = {
+    protected def sameName(annot: Symbol)(cand: Name) = cand == annot.name
+    protected def hasAnnotation(d: DefDef, annot: Symbol): Boolean = {
       d.mods.annotations.find { a ⇒
         val n = annotationName(a)
         n.filter(sameName(annot)).isDefined
       }.isDefined
     }
-    def hasAnnotation(t: Tree, annot: Symbol): Boolean = t match {
+    protected def hasAnnotation(c: ClassDef, annot: Symbol): Boolean = {
+      c.mods.annotations.find { a ⇒
+        val n = annotationName(a)
+        n.filter(sameName(annot)).isDefined
+      }.isDefined
+    }
+    protected def hasAnnotation(t: Tree, annot: Symbol): Boolean = t match {
       case Annotated(a, t) if annotationName(a).filter(sameName(annot)).isDefined ⇒ true
       case Annotated(a, t) ⇒ hasAnnotation(t, annot)
       case _ ⇒ false
     }
-    def annotate(t: Tree, a: Symbol): Tree = {
-      Annotated(makeAnnotation(a), t)
-    }
+    protected def annotate(t: Tree, a: Symbol): Tree = Annotated(makeAnnotation(a), t)
+
+    protected def isConstructor(d: DefDef) = d.name.toString == "<init>"
+
+    private var impureClass: Boolean = false
 
     override def transform(tree: Tree): Tree = tree match {
       case d: DefDef if hasAnnotation(d, Annotation.impure) && !hasAnnotation(d.tpt, Annotation.sideEffect) ⇒
         val ntpt = annotate(d.tpt, Annotation.sideEffect)
         super.transform(d.copy(tpt = ntpt))
+      case d: DefDef if isConstructor(d) && !hasAnnotation(d.tpt, Annotation.sideEffect) && impureClass ⇒
+        val ntpt = annotate(d.tpt, Annotation.sideEffect)
+        super.transform(d.copy(tpt = ntpt))
+      case c: ClassDef ⇒
+        impureClass = hasAnnotation(c, Annotation.impure)
+        super.transform(c)
       case other ⇒
         super.transform(tree)
     }
