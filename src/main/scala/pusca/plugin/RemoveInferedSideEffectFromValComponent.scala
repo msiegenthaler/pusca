0e4ebb8ef81c93cb1b720b40c7553ae60fb1ed51
msiegenthaler <msiegenthaler@gmx.net>
2011-10-17 20:11:41 +0200

    also remove side-effect from method call paramters

--- src/main/scala/pusca/plugin/RemoveInferedSideEffectFromValComponent.scala
+++ src/main/scala/pusca/plugin/RemoveInferedSideEffectFromValComponent.scala
@@ -14,17 +14,22 @@ class RemoveInferedSideEffectFromValComponent(val global: Global) extends Plugin
   def newTransformer(unit: CompilationUnit) = new RemoveInferedSideEffectFromVal
 
   class RemoveInferedSideEffectFromVal extends Transformer {
-    protected def applySideEffect(v: Tree) = Apply(Select(Select(Ident("pusca"), "package"), "applySideEffect"), v :: Nil)
-//    protected def applySideEffect(v: Tree) = v
+    protected def applySideEffectFun = Select(Select(Ident("pusca"), "package"), "applySideEffect")
+    protected def applySideEffect(v: Tree) = Apply(applySideEffectFun, v :: Nil)
 
     override def transform(tree: Tree): Tree = tree match {
       //handle calls inside impure
       case v @ ValDef(_, _, TypeTree(), rhs) ⇒ //val/var without explicitly specified type
         v.copy(rhs = applySideEffect(transform(rhs)))
 
+      case Apply(Apply(applySideEffectFun, _), args) ⇒ //method call to applySideEffect
+        super.transform(tree)
+      case Apply(fun, args) ⇒ // method calls
+        Apply(transform(fun), args.map(transform).map(applySideEffect))
+
       case other ⇒ super.transform(other)
     }
   }
-  
+
   //TODO remove unused calls in a later phase (after the typer)
 }
