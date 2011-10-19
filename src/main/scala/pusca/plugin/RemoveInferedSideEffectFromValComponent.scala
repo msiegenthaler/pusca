2f1dbddfac2f3a07ce860144e2a555843cc2a8a9
msiegenthaler <msiegenthaler@gmx.net>
2011-10-19 21:24:20 +0200

    rewrite to have a higher hit rate (previously missed unassigned function calls inside a method)

--- src/main/scala/pusca/plugin/RemoveInferedSideEffectFromValComponent.scala
+++ src/main/scala/pusca/plugin/RemoveInferedSideEffectFromValComponent.scala
@@ -15,7 +15,11 @@ class RemoveInferedSideEffectFromValComponent(val global: Global) extends Plugin
 
   class RemoveInferedSideEffectFromVal extends Transformer {
     protected def applySideEffectFun = Select(Ident("pusca"), "applySideEffect")
-    protected def applySideEffect(v: Tree) = Apply(applySideEffectFun, v :: Nil)
+    protected def applySideEffect(v: Tree) = {
+      val a = Apply(applySideEffectFun, v :: Nil)
+      a.pos = v.pos
+      a
+    }
 
     protected object ApplySideEffectFun {
       def unapply(t: Tree) = t match {
@@ -27,20 +31,78 @@ class RemoveInferedSideEffectFromValComponent(val global: Global) extends Plugin
       }
     }
 
-    override def transform(tree: Tree): Tree = tree match {
-      //val/var without explicitly specified type
-      case v @ ValDef(_, _, TypeTree(), ApplySideEffectFun(_)) ⇒
-        super.transform(v)
-      case v @ ValDef(_, _, TypeTree(), rhs) ⇒
-        v.copy(rhs = applySideEffect(transform(rhs)))
+    override def transform(tree: Tree): Tree = findStatementBlock(tree)
 
-      //method calls
-      case a @ ApplySideEffectFun(_) ⇒ //method call to applySideEffect
-        super.transform(a)
-      case Apply(fun, args) ⇒ // other method calls
-        Apply(transform(fun), args.map(transform).map(applySideEffect))
+    protected def findStatementBlock(t: Tree): Tree = t match {
+      case t: Template ⇒
+        val body = t.body.map(handleStatementBlock)
+        treeCopy.Template(t, t.parents, t.self, body)
+      case d: DefDef ⇒
+        val rhs = handleStatementBlock(d.rhs)
+        treeCopy.DefDef(d, d.mods, d.name, d.tparams, d.vparamss, d.tpt, rhs)
+      case v: ValDef ⇒
+        val rhs = handleStatementBlock(v.rhs)
+        treeCopy.ValDef(v, v.mods, v.name, v.tpt, rhs)
+      case b: Block ⇒
+        val expr = handleStatementBlock(b.expr)
+        val stats = b.stats.map(handleStatementBlock)
+        treeCopy.Block(b, stats, expr)
 
       case other ⇒ super.transform(other)
     }
+
+    protected def handleStatementBlock(t: Tree): Tree = t match {
+      case a @ ApplySideEffectFun(args) ⇒ //method call to applySideEffect
+        a
+      case a @ Apply(Select(Super(_, _), n), _) if n == stringToTermName("<init>") ⇒ // don't do anything with super.<init>
+        a
+      case a @ Apply(fun, args) ⇒ // other method calls
+        val nargs = args.map(handleStatementBlock)
+        applySideEffect(treeCopy.Apply(a, fun, nargs))
+
+      case i: Ident ⇒ //only the outermost
+        applySideEffect(i)
+
+      case other ⇒ findStatementBlock(other)
+    }
   }
+
+  //    private var inRelevant = false
+  //    private def isRelevant: Boolean = inRelevant
+  //    private def relevant[A](f: ⇒ A): A = {
+  //      inRelevant = true
+  //      val r = f
+  //      inRelevant = false
+  //      r
+  //    }
+  //
+  //    override def transform(tree: Tree): Tree = tree match {
+  //      case d: DefDef ⇒
+  //        val nrhs = relevant(transform(d.rhs))
+  //        d.copy(rhs = nrhs)
+  //      case c: ClassDef ⇒
+  //        c.copy(impl = transform(c.impl).asInstanceOf[Template])
+  //      case t: Template ⇒
+  //        val nb = relevant(t.body.map(transform))
+  //        t.copy(body = nb)
+  //      case b: Block ⇒ relevant {
+  //        b.copy(expr = transform(b.expr), stats = b.stats.map(transform))
+  //      }
+  //
+  //      //val/var without explicitly specified type
+  //      case v @ ValDef(_, _, TypeTree(), ApplySideEffectFun(_)) if isRelevant ⇒
+  //        super.transform(v)
+  //      case v @ ValDef(_, _, TypeTree(), rhs) if isRelevant ⇒
+  //        v.copy(rhs = applySideEffect(transform(rhs)))
+  //
+  //      //method calls
+  //      case a @ ApplySideEffectFun(_) ⇒ //method call to applySideEffect
+  //        super.transform(a)
+  //      case Apply(fun, args) if isRelevant ⇒ // other method calls
+  //        applySideEffect(Apply(transform(fun), args.map(transform).map(applySideEffect)))
+  //
+  //      case other ⇒
+  //        super.transform(other)
+  //    }
+  //  }
 }
