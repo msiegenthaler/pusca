8fe0eb5b2a90bda05ef08f00da6652b10127f8c1
msiegenthaler <msiegenthaler@gmx.net>
2011-10-22 10:48:32 +0200

    don't touch unspecified return types but instead wrapp the body in addSideEffect()

--- src/main/scala/pusca/package.scala
+++ src/main/scala/pusca/package.scala
@@ -4,4 +4,6 @@ package object pusca {
   /** removes the @sideEffect annotation */
   def applySideEffect[A](a: A @sideEffect): A = a.asInstanceOf[A]
   
+  /** adds an @sideEffect annotation */
+  def addSideEffect[A](a: A): A @sideEffect = a
 }
--- src/main/scala/pusca/plugin/MethodReturnTypeAnnotatorComponent.scala
+++ src/main/scala/pusca/plugin/MethodReturnTypeAnnotatorComponent.scala
@@ -17,18 +17,35 @@ class MethodReturnTypeAnnotatorComponent(val global: Global) extends PluginCompo
   def newTransformer(unit: CompilationUnit) = new MethodReturnTypeAnnotator
 
   class MethodReturnTypeAnnotator extends Transformer {
-    private var impureClass: Boolean = false
+    protected def addSideEffectFun = Select(Ident("pusca"), "addSideEffect")
+    protected def addSideEffect(v: Tree) = {
+      val a = Apply(addSideEffectFun, v :: Nil)
+      a.pos = v.pos
+      a
+    }
 
+    private var impureClass: Boolean = false
     override def transform(tree: Tree): Tree = tree match {
-      case d: DefDef if hasAnnotation(d, Annotation.impure) && !hasAnnotation(d.tpt, Annotation.sideEffect) ⇒
-        val ntpt = annotate(d.tpt, Annotation.sideEffect)
-        super.transform(d.copy(tpt = ntpt))
       case d: DefDef if isConstructor(d) && !hasAnnotation(d.tpt, Annotation.sideEffect) && impureClass ⇒
         val ntpt = annotate(d.tpt, Annotation.sideEffect)
         super.transform(d.copy(tpt = ntpt))
+      case d @ DefDef(_, _, _, _, tpt, rhs) if hasAnnotation(d, Annotation.impure) && !hasAnnotation(d.tpt, Annotation.sideEffect) ⇒ tpt match {
+        case TypeTree() ⇒ //unspecified return type 
+          val nrhs = rhs match {
+            case b @ Block(stats, expr) ⇒ treeCopy.Block(b, stats.map(transform), addSideEffect(transform(expr)))
+            case other                  ⇒ addSideEffect(transform(other))
+          }
+          treeCopy.DefDef(d, d.mods, d.name, d.tparams, d.vparamss, tpt, nrhs)
+
+        case tpt ⇒
+          val ntpt = annotate(d.tpt, Annotation.sideEffect)
+          super.transform(d.copy(tpt = ntpt))
+      }
+
       case c: ClassDef ⇒
         impureClass = hasAnnotation(c, Annotation.impure)
         super.transform(c)
+
       case other ⇒
         super.transform(tree)
     }
