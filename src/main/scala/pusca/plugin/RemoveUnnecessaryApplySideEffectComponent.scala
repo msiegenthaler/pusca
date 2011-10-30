1998e61939697e4dbf9df718f47d117dd8108be3
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-10-30 10:36:31 +0100

    drop dependency on (no longer existing) applySideEffect phase

--- src/main/scala/pusca/plugin/RemoveUnnecessaryApplySideEffectComponent.scala
+++ src/main/scala/pusca/plugin/RemoveUnnecessaryApplySideEffectComponent.scala
@@ -6,14 +6,15 @@ import scala.tools.nsc.Global
 import scala.tools.nsc.Phase
 
 /**
- * Removes all unnecessary calls to applySideEffect (inserted by the applySideEffect phase).
+ * Removes all unnecessary calls to applySideEffect (inserted by SideEffectChecker).
  * The reason they get inserted and then later removed is, that the information to decide whether they were necessary is only
  * available after the typer has run.
  */
+//TODO check if this phase is still useful
 class RemoveUnnecessaryApplySideEffectComponent(val global: Global) extends PluginComponent with Transform with PuscaDefinitions with ParserStageSupport {
   import global._
 
-  val runsAfter = List("typer", "applySideEffect")
+  val runsAfter = List("typer")
   val phaseName = "removeUnnecessaryApplySideEffect"
   def newTransformer(unit: CompilationUnit) = new RemoveUnnecessaryApplySideEffect
 
