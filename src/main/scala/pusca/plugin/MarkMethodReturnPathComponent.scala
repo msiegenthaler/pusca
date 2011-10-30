55bb66c65b77c1fce3a0f5287a74c5d0bac259d7
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-10-30 21:37:36 +0100

    only do the return path marking in methods with infered  return type

--- src/main/scala/pusca/plugin/MarkMethodReturnPathComponent.scala
+++ src/main/scala/pusca/plugin/MarkMethodReturnPathComponent.scala
@@ -16,10 +16,10 @@ class MarkMethodReturnPathComponent(val global: Global) extends PluginComponent 
 
   class MarkMethodReturnPath extends Transformer {
     override def transform(tree: Tree): Tree = tree match {
-      case d: DefDef if !hasAnnotation(d, Annotation.pure) ⇒
+      case d @ DefDef(mods, name, tparams, vparamss, TypeTree(), rhs) if !hasAnnotation(d, Annotation.pure) ⇒
         val needToAddSideEffect = hasAnnotation(d, Annotation.impure) && !hasAnnotation(d.tpt, Annotation.sideEffect)
-        val nrhs = transformReturn(d.rhs, needToAddSideEffect)
-        treeCopy.DefDef(d, d.mods, d.name, d.tparams, d.vparamss, d.tpt, nrhs)
+        val nrhs = transformReturn(rhs, needToAddSideEffect)
+        treeCopy.DefDef(d, mods, name, tparams, vparamss, d.tpt, nrhs)
       case f: Function ⇒
         val nb = transformReturn(f.body, false)
         treeCopy.Function(f, f.vparams, nb)
