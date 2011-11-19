a8f5ede636f40f8341b4735c7087c3d73eaadc6c
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-11-19 18:13:19 +0100

    first shot at an implementation

--- src/main/scala/pusca/plugin/ParametrizedTypesApplicatorComponent.scala
+++ src/main/scala/pusca/plugin/ParametrizedTypesApplicatorComponent.scala
@@ -21,48 +21,166 @@ class ParametrizedTypesApplicatorComponent(val global: Global) extends PluginCom
     private val symbolName = stringToTermName("Symbol")
     private val scalaName = stringToTermName("scala")
     def unapply(t: Tree) = t match {
-      case Apply(Select(Select(Ident(scalaName), symbolName), applyName), Literal(arg @ Constant(_)) :: Nil) if arg.tag == StringTag ⇒ Some(arg.stringValue)
+      case Apply(Select(Select(Ident(scalaName), symbolName), applyName), Literal(arg @ Constant(_)) :: Nil) if arg.tag == StringTag ⇒
+        Some(arg.stringValue.intern)
+      case _ ⇒ None
+    }
+  }
+  private object WithTypeVar {
+    def unapply(t: Tree) = t.tpe match {
+      case TypeRef(_, s, _) if s.isTypeParameterOrSkolem ⇒ Some(s.nameString)
       case _ ⇒ None
     }
   }
 
+  //*** Start *****
+
+  sealed trait Purity
+  case object AlwaysPure extends Purity
+  case object AlwaysImpure extends Purity
+  case class ImpureDependingOn(tparams: Set[String]) extends Purity
+
+  /** Determines the declared purity of a method. Does not take the body of the method into account, whether the body conforms to the declaration is checked elsewhere. */
+  def methodPurity(s: Symbol): Purity = s match {
+    case s: MethodSymbol if hasAnnotation(s, Annotation.pure) ⇒
+      AlwaysPure
+    case s: MethodSymbol if hasAnnotation(s, Annotation.impure) ⇒
+      AlwaysImpure
+    case s: MethodSymbol if hasAnnotation(s, Annotation.impureIf) ⇒
+      val impureIfs = s.annotations.find(_.atp.typeSymbol == Annotation.impureIf) match {
+        case Some(AnnotationInfo(_, args, _)) ⇒ args.collect { case SymbolApply(arg) ⇒ arg }
+        case None                             ⇒ Nil
+      }
+      ImpureDependingOn(impureIfs.toSet)
+    case s: MethodSymbol ⇒ //impureIfImpureResult
+      val rt = resultType(s)
+      if (rt.typeSymbol.isTypeParameterOrSkolem) ImpureDependingOn(Set(rt.typeSymbol.name.toString))
+      else AlwaysPure
+    case _ ⇒
+      AlwaysPure
+  }
+  private def resultType(t: MethodSymbol): Type = {
+    def resTpe(t: Type): Type = t match {
+      case PolyType(_, rt)      ⇒ resTpe(rt)
+      case MethodType(_, r)     ⇒ r
+      case NullaryMethodType(r) ⇒ r
+    }
+    resTpe(t.tpe)
+  }
+
+  /**
+   * Gets the values used for the type parameters of a method.
+   * Example:
+   *  for
+   *  <code>
+   * 	trait Example[B] {
+   * 		def method1[A](a: A): B
+   *  }
+   *  new Example[String].method1(10)
+   *  </code>
+   * it returns: A -> Int, B -> String
+   */
+  def resolveTypeParams(a: Apply): Map[String, Type] = {
+    val o = a.fun.symbol.owner
+    println("owner of " + a.fun + " = " + o)
+    println("ot = " + o.typeParams)
+    def methodOwnerTypeParams(a: Tree): Map[String, Type] = a match {
+      case Select(o, _) ⇒
+        o.tpe.underlying match {
+          case TypeRef(p, s, a) ⇒
+            s.typeParams.map(_.name.toString).zip(a).toMap
+          case _ ⇒ Map()
+        }
+      case _ ⇒ Map()
+    }
+
+    a.fun match {
+      //TODO also resolve from outer classes/defs of fun
+      case TypeApply(fun, targs) ⇒
+        val pl = fun.symbol.typeParams.map(_.name.toString).toList
+        methodOwnerTypeParams(fun) ++ pl.zip(targs.map(_.tpe)).toMap
+      case f ⇒ methodOwnerTypeParams(f)
+    }
+  }
+
+  /** checks whether an apply is pure given the list of with names of type parameters that are allowed to be impure. */
+  def violatesPurity(a: Apply, allowedImpures: Set[String] = Set()): Boolean = methodPurity(a.fun.symbol) match {
+    case AlwaysPure   ⇒ false
+    case AlwaysImpure ⇒ true
+    case ImpureDependingOn(di) ⇒
+      def mayHaveSideEffect(tpe: Type) = hasSideEffect(tpe) || tpe.typeSymbol.isTypeParameterOrSkolem
+      def isAllowedImpure(tpe: Type) = tpe.typeSymbol.isTypeParameterOrSkolem || allowedImpures.contains(tpe.typeSymbol.name.toString)
+
+      val tparams = resolveTypeParams(a).filter(e ⇒ di.contains(e._1))
+      di.filterNot(tparams.contains).foreach { p ⇒ reporter.error(a.pos, "unresolved type parameter " + p + " on call to " + a.fun.symbol.fullName) }
+      val ips = tparams.filter(e ⇒ mayHaveSideEffect(e._2) && !isAllowedImpure(e._2))
+      ips.foreach(i ⇒ println("@@  unallowed impure: " + i)) //TODO
+      ips.nonEmpty
+  }
   private class Looker(unit: CompilationUnit) extends TypingTransformer(unit) {
     override def transform(tree: Tree): Tree = tree match {
       case d @ DefDef(mods, name, tparams, vparamss, tpt, rhs) ⇒
-        val impureIf = d.symbol.annotations.find(_.atp.typeSymbol == Annotation.impureIf) match {
-          case Some(AnnotationInfo(_, args, _)) ⇒ args.collect { case SymbolApply(arg) ⇒ arg }
-          case None                             ⇒ Nil
+        methodPurity(d.symbol) match {
+          case AlwaysPure ⇒
+            val appl = new Applicator(unit, d.symbol, Set())
+            treeCopy.DefDef(d, mods, name, tparams, vparamss, tpt, appl.transform(d.rhs))
+          case AlwaysImpure ⇒
+            treeCopy.DefDef(d, mods, name, tparams, vparamss, tpt, transform(d.rhs))
+          case ImpureDependingOn(impures) ⇒
+            val appl = new Applicator(unit, d.symbol, impures)
+            treeCopy.DefDef(d, mods, name, tparams, vparamss, tpt, appl.transform(d.rhs))
         }
-        //println("found defdef " + d.name + "  with impureIfs: " + impureIf) //TODO
-        val appl = new Applicator(unit, impureIf.toSet)
-        treeCopy.DefDef(d, mods, name, tparams, vparamss, tpt, appl.transform(d.rhs))
+      //        val impureIf = d.symbol.annotations.find(_.atp.typeSymbol == Annotation.impureIf) match {
+      //          case Some(AnnotationInfo(_, args, _)) ⇒ args.collect { case SymbolApply(arg) ⇒ arg }
+      //          case None                             ⇒ Nil
+      //        }
+      //println("found defdef " + d.name + "  with impureIfs: " + impureIf) //TODO
+      //        val appl = new Applicator(unit, d.symbol, impureIf.map(_.toString).toSet)
+      //        treeCopy.DefDef(d, mods, name, tparams, vparamss, tpt, appl.transform(d.rhs))
       //TODO ClassDef
-      //TODO Function
+      //TODO Function (or shift after uncurry?)
 
       case other ⇒ super.transform(other)
     }
 
-    private class Applicator(unit: CompilationUnit, impures: Set[String]) extends TypingTransformer(unit) {
-      private object WithTypeVar {
-        def unapply(t: Tree) = t.tpe match {
-          case TypeRef(_, s, _) if s.isTypeParameterOrSkolem ⇒ Some(s.nameString)
-          case _ ⇒ None
-        }
-      }
+    private class Applicator(unit: CompilationUnit, method: Symbol, impures: Set[String]) extends TypingTransformer(unit) {
       override def transform(tree: Tree): Tree = tree match {
-        case a @ ApplySideEffect(_) ⇒ a
-        case a @ AddSideEffect(_)   ⇒ a
-        case a: Apply ⇒ a match {
-          case WithTypeVar(v) ⇒
-            println("found var: " + v)
-            if (impures.contains(v)) {
-              println("is allowed to be impure")
-              super.transform(a)
-            } else {
-              localTyper.typed(applySideEffect(a))
-            }
-          case _ ⇒ super.transform(a)
-        }
+        //        case a @ ApplySideEffect(_) ⇒ a
+        //        case a @ AddSideEffect(_)   ⇒ a
+
+        //        case a: Apply ⇒ a match {
+        //          //          case WithTypeVar(v) ⇒
+        //          //            println("found var: " + v)
+        //          //            if (impures.contains(v)) {
+        //          //              println("is allowed to be impure")
+        //          //              super.transform(a)
+        //          //            } else {
+        //          //              localTyper.typed(applySideEffect(a))
+        //          //            }
+        //          case x if x.fun.toString.contains("exec") ⇒
+        //            val tpe = x.fun.tpe
+        //            val fs = x.fun.symbol
+        //            println("@fs   " + fs)
+        //            println("@tpe  " + tpe)
+        //
+        //            val r = methodPurity(fs)
+        //            println("@r    " + r)
+        //
+        //            println("@tparams " + a.fun.symbol.typeParams)
+        //            val xs = x.symbol
+        //            val xst = xs.typeParams
+        //            println("@xs " + xs)
+        //            println("@xst " + xst)
+        //
+        //            val rtp = resolveTypeParams(x)
+        //            println("@ rtp = " + rtp)
+        //
+        //            println("@ impures for  " + x + " = " + impures)
+        //            println("@ is pure apply: " + isPureApply(x, impures))
+        //            
+        case a: Apply if violatesPurity(a, impures) ⇒
+          reporter.error(a.pos, "impure method call inside the pure method '" + method.fullName + "'") //TODO
+          super.transform(a)
 
         case d: DefDef   ⇒ Looker.this.transform(d)
         case c: ClassDef ⇒ Looker.this.transform(c)
