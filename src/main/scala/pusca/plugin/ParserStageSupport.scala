3c8f0a7cc01fe37aaa9ff401c2c76eb08dc7381b
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-12-10 16:33:21 +0100

    slight refactoring to eliminate duplicate code

--- src/main/scala/pusca/plugin/ParserStageSupport.scala
+++ src/main/scala/pusca/plugin/ParserStageSupport.scala
@@ -5,31 +5,23 @@ private[plugin] trait ParserStageSupport {
   val global: Global
   import global._
 
-  protected def annotationName(t: Tree): Option[Name] = t match {
+  private def sameName(annot: Symbol)(cand: Name) = cand == annot.name
+  private def annotationName(t: Tree): Option[Name] = t match {
     case Apply(Select(New(Ident(tpe)), method), args) ⇒ Some(tpe)
     case _ ⇒ None
   }
-  protected def makeAnnotation(annot: Symbol): Tree = {
-    Apply(Select(New(Ident(annot.name)), mkTermName("<init>")), Nil)
-  }
+  private def findAnnotation(a: Symbol, annots: List[Tree]) = annots.view.map(annotationName).flatten.find(sameName(a)).isDefined
 
-  protected def sameName(annot: Symbol)(cand: Name) = cand == annot.name
-  protected def hasAnnotation(d: DefDef, annot: Symbol): Boolean = {
-    d.mods.annotations.find { a ⇒
-      val n = annotationName(a)
-      n.filter(sameName(annot)).isDefined
-    }.isDefined
-  }
-  protected def hasAnnotation(c: ClassDef, annot: Symbol): Boolean = {
-    c.mods.annotations.find { a ⇒
-      val n = annotationName(a)
-      n.filter(sameName(annot)).isDefined
-    }.isDefined
-  }
+  protected def hasAnnotation(d: DefDef, annot: Symbol): Boolean = findAnnotation(annot, d.mods.annotations)
+  protected def hasAnnotation(c: ClassDef, annot: Symbol): Boolean = findAnnotation(annot, c.mods.annotations)
   protected def hasAnnotation(t: Tree, annot: Symbol): Boolean = t match {
-    case Annotated(a, t) if annotationName(a).filter(sameName(annot)).isDefined ⇒ true
+    case Annotated(a, t) if findAnnotation(annot, List(a)) ⇒ true
     case Annotated(a, t) ⇒ hasAnnotation(t, annot)
     case _ ⇒ false
+  }
+
+  protected def makeAnnotation(annot: Symbol): Tree = {
+    Apply(Select(New(Ident(annot.name)), mkTermName("<init>")), Nil)
   }
   protected def annotate(t: Tree, a: Symbol): Tree = Annotated(makeAnnotation(a), t)
 
