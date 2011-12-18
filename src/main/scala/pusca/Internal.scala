b4617f0760c1411c9c1afca59b81c8773f93e4d4
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-12-18 21:06:06 +0100

    also add @sideEffect/@sideEffectFree on the markers

--- src/main/scala/pusca/Internal.scala
+++ src/main/scala/pusca/Internal.scala
@@ -7,8 +7,8 @@ import scala.annotation.TypeConstraint
  */
 object Internal {
   def markInfere[A](a: A): A @returnedInfere = throw new UnsupportedOperationException
-  def markSideEffect[A](a: A): A @returnedSideEffect = throw new UnsupportedOperationException
-  def markSideEffectFree[A](a: A): A @returnedSideEffectFree = throw new UnsupportedOperationException
+  def markSideEffect[A](a: A): A @returnedSideEffect @sideEffect = throw new UnsupportedOperationException
+  def markSideEffectFree[A](a: A): A @returnedSideEffectFree @sideEffectFree = throw new UnsupportedOperationException
 
   class returnedInfere extends StaticAnnotation with TypeConstraint
   class returnedSideEffect extends StaticAnnotation with TypeConstraint
