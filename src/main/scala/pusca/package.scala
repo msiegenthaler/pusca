8ea419e87311bb2d361636408da2af295699110e
Mario Siegenthaler <msiegenthaler@gmx.net>
2011-12-18 11:25:49 +0100

    more robust definition of PureFunction (because of the compiler plugin with Pure[A])

--- src/main/scala/pusca/package.scala
+++ src/main/scala/pusca/package.scala
@@ -7,11 +7,11 @@ package object pusca {
   type Impure[+A] = A @sideEffect
 
   /** Function that evaluates without a side effect. */
-  type PureFunction[-V1, +R] = Function1[V1, Pure[R]]
+  type PureFunction[-V1, +R] = Function1[V1, R @sideEffectFree]
   type ->[-V1, +R] = PureFunction[V1, R]
 
   /** Function that evaluates without a side effect. */
-  type ImpureFunction[-V1, +R] = Function1[V1, Impure[R]]
+  type ImpureFunction[-V1, +R] = Function1[V1, R @sideEffect]
   type ==>[-V1, +R] = ImpureFunction[V1, R]
 
 
