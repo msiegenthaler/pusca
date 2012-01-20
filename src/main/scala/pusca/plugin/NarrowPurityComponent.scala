41dbf7ec8e03cefc2d1d2ef57f43a438356fee55
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-01-20 20:22:40 +0100

    remove unused code

--- src/main/scala/pusca/plugin/NarrowPurityComponent.scala
+++ src/main/scala/pusca/plugin/NarrowPurityComponent.scala
@@ -46,43 +46,4 @@ class NarrowPurityComponent(val global: Global) extends PluginComponent with Pus
       super.transform(nt)
     }
   }
-  /*
-  def newPhase(prev: Phase) = new NarrowPurity(prev)
-
-  class NarrowPurity(prev: Phase) extends StdPhase(prev) {
-
-    @tailrec private def returnTypeOf(methodType: Type): Type = methodType match {
-      case PolyType(_, rt)      ⇒ returnTypeOf(rt)
-      case MethodType(_, r)     ⇒ r
-      case NullaryMethodType(r) ⇒ r
-      case t                    ⇒ t
-    }
-
-    private def resolveOverridenType(implementor: Symbol)(baseMethod: Symbol) = implementor.tpe.memberInfo(baseMethod)
-
-    def hasSideEffectFreeReturn(d: DefDef): Boolean = hasSideEffectFreeReturn(d.symbol)
-    def hasSideEffectFreeReturn(method: Symbol): Boolean = {
-      method.extendedOverriddenSymbols.view.map(resolveOverridenType(method.owner)).map(returnTypeOf).
-        filter(SideEffectFreeType.isSideEffectFree).nonEmpty
-    }
-
-    override def apply(unit: CompilationUnit) {
-      def look(t: Tree): Unit = {
-        t match {
-          case d: DefDef if d.symbol.hasAnnotation(Annotation.impureIfReturnType) && hasSideEffectFreeReturn(d) ⇒
-            // TODO: write a component that replaces @impureIfReturnType with @pure for this case 
-            println("must be pure: " + d.symbol.fullName)
-
-          //TODO: should we switch to @pure if a method overriding a method annotated with @impureIfReturnType
-          // and a non skolem/param return type? probably yes..
-          // --> we probably need to do that anyway, not only on overriden methods..
-
-          case _ ⇒ ()
-        }
-        t.children.foreach(look)
-      }
-      look(unit.body)
-    }
-  }
-  */
 }
